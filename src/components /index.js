aac7be2d1cdee807935c388b8a66f46296875694
RuslanBakhtiyarov <bakhtiyarovruslan1@gmail.com>
2021-09-15 14:55:48 +0300

    my-app2

---
+++ src/components /index.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+const AppHeader = () => {
+  return (
+    <ul>
+      <li>title</li>
+      <li>descr</li>
+    </ul>
+  );
+}
+const AppLayout = () => {
+  return (
+    <ul>
+      <li>title</li>
+      <li>descr</li>
+      <li>urlBg</li>
+      <li>colorBg</li>
+    </ul>
+  );
+}
+const AppFooter = () => {
+  return (<h1>hello</h1>
+  );
+}
+const el = ( 
+  <>
+    <AppHeader />
+    <AppLayout />
+  <AppFooter/>
+  </>
+
+);
+
+  
